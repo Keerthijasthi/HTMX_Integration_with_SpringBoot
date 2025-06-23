7c32e634405bd6b2d987fffc0e050b056842eccf
Keerthijasthi <keerthi.jasthi4@gmail.com>
2025-06-23 12:22:15 -0500

    Initial commit

---
+++ src/main/java/com/example/Springboot_Project_Setup/SpringbootProjectSetupApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springboot_Project_Setup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProjectSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProjectSetupApplication.class, args);
+	}
+
+}
